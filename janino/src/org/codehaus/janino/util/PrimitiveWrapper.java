d6a0e7c29feaf24994b9f98640069891fbca0700
aunkrig <aunkrig@eb4544d6-894b-0410-9319-a9612837a279>
2005-01-31 21:34:41 +0000

    Fixed "JANINO-4 No net.janino.util.PrimitiveWrapper.wrap(boolean v) available ...".

--- janino/src/org/codehaus/janino/util/PrimitiveWrapper.java
+++ janino/src/org/codehaus/janino/util/PrimitiveWrapper.java
@@ -38,12 +38,13 @@ package org.codehaus.janino.util;
  * A helper class that wraps primitive values in their wrapper classes.
  */
 public class PrimitiveWrapper {
-    public static Byte      wrap(byte   v) { return new Byte(v);      }
-    public static Short     wrap(short  v) { return new Short(v);     }
-    public static Integer   wrap(int    v) { return new Integer(v);   }
-    public static Long      wrap(long   v) { return new Long(v);      }
-    public static Character wrap(char   v) { return new Character(v); }
-    public static Float     wrap(float  v) { return new Float(v);     }
-    public static Double    wrap(double v) { return new Double(v);    }
-    public static Object    wrap(Object v) { return v;                }
+    public static Boolean   wrap(boolean v) { return new Boolean(v);   }
+    public static Byte      wrap(byte    v) { return new Byte(v);      }
+    public static Short     wrap(short   v) { return new Short(v);     }
+    public static Integer   wrap(int     v) { return new Integer(v);   }
+    public static Long      wrap(long    v) { return new Long(v);      }
+    public static Character wrap(char    v) { return new Character(v); }
+    public static Float     wrap(float   v) { return new Float(v);     }
+    public static Double    wrap(double  v) { return new Double(v);    }
+    public static Object    wrap(Object  v) { return v;                }
 }
