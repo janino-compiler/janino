2adc20a730abf368d5e9b59432e34b492f80934b
aunkrig <aunkrig@eb4544d6-894b-0410-9319-a9612837a279>
2010-03-10 21:23:17 +0000

    The constructor "RuntimeException(Throwable cause)" and its relatives does not exist in JDK 1.3, and JANINO ist (still) Java 1.3 compatible!

--- janino/src/org/codehaus/janino/JaninoRuntimeException.java
+++ janino/src/org/codehaus/janino/JaninoRuntimeException.java
@@ -5,8 +5,6 @@ package org.codehaus.janino;
  * to allow for client libraries to intercept them more easily. 
  */
 public class JaninoRuntimeException extends RuntimeException {
-    public JaninoRuntimeException()                                {                        }
-    public JaninoRuntimeException(String message)                  { super(message);        }
-    public JaninoRuntimeException(Throwable cause)                 { super(cause);          }
-    public JaninoRuntimeException(String message, Throwable cause) { super(message, cause); }
+    public JaninoRuntimeException()               {                 }
+    public JaninoRuntimeException(String message) { super(message); }
 }
