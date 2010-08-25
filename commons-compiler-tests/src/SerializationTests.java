3055dcb47642abb330d06098a24188d34e93ef31
aunkrig <aunkrig@eb4544d6-894b-0410-9319-a9612837a279>
2010-08-25 11:27:55 +0000

    Fixed th copyright year.

--- commons-compiler-tests/src/SerializationTests.java
+++ commons-compiler-tests/src/SerializationTests.java
@@ -2,7 +2,7 @@
 /*
  * Janino - An embedded Java[TM] compiler
  *
- * Copyright (c) 2001-2007, Arno Unkrig
+ * Copyright (c) 2001-2010, Arno Unkrig
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
