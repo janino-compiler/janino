43e4d694c1fb11c320db34b1af2b9dca9719e053
aunkrig <aunkrig@eb4544d6-894b-0410-9319-a9612837a279>
2013-11-07 10:30:06 +0000

    JAVADOC.

--- janino/src/org/codehaus/janino/Mod.java
+++ janino/src/org/codehaus/janino/Mod.java
@@ -36,41 +36,158 @@ public final
 class Mod {
     private Mod() {} // Don't instantiate me!
 
-    public static final short NONE         = 0x0000;
+    /** An alias for '0' -- <i>no</i> modifiers. */
+    public static final short NONE = 0x0000;
 
-    public static final short PUBLIC       = 0x0001;
-    public static final short PRIVATE      = 0x0002;
-    public static final short PROTECTED    = 0x0004;
-    public static final short PACKAGE      = 0x0000;
-    public static final short PPP          = 0x0007;
+    /**
+     * The flag indicating 'public accessibility' of the modified element. Methods of interfaces are always {@link
+     * #PUBLIC}.
+     *
+     * @see #PPP
+     * @see #isPublicAccess(short)
+     */
+    public static final short PUBLIC = 0x0001;
+    
+    /**
+     * The flag indicating 'private accessibility' of the modified element.
+     *
+     * @see #PPP
+     * @see #isPrivateAccess(short)
+     */
+    public static final short PRIVATE = 0x0002;
+    
+    /**
+     * The flag indicating 'protected accessibility' of the modified element.
+     *
+     * @see #PPP
+     * @see #isProtectedAccess(short)
+     */
+    public static final short PROTECTED = 0x0004;
+    
+    /**
+     * The flag indicating 'default accessibility' a.k.a. 'package accessibility' of the modified element.
+     *
+     * @see #PPP
+     * @see #isPackageAccess(short)
+     */
+    public static final short PACKAGE = 0x0000;
+    
+    /** The mask to select the accessibility flags from modifiers. */
+    public static final short PPP = 0x0007;
 
-    public static boolean isPublicAccess(short sh)    { return (sh & Mod.PPP) == Mod.PUBLIC; }
-    public static boolean isPrivateAccess(short sh)   { return (sh & Mod.PPP) == Mod.PRIVATE; }
+    /** @return Whether the given modifier symbolizes PUBLIC accessibility */
+    public static boolean isPublicAccess(short sh) { return (sh & Mod.PPP) == Mod.PUBLIC; }
+    
+    /** @return Whether the given modifier symbolizes PRIVATE accessibility */
+    public static boolean isPrivateAccess(short sh) { return (sh & Mod.PPP) == Mod.PRIVATE; }
+    
+    /** @return Whether the given modifier symbolizes PROTECTED accessibility */
     public static boolean isProtectedAccess(short sh) { return (sh & Mod.PPP) == Mod.PROTECTED; }
-    public static boolean isPackageAccess(short sh)   { return (sh & Mod.PPP) == Mod.PACKAGE; }
+    
+    /** @return Whether the given modifier symbolizes PACKAGE (or 'default') accessibility */
+    public static boolean isPackageAccess(short sh) { return (sh & Mod.PPP) == Mod.PACKAGE; }
 
+    /** @return The given {@code modifiers}, but with the accessibility part changed to {@code newAccess} */
     public static short
     changeAccess(short modifiers, short newAccess) { return (short) ((modifiers & ~Mod.PPP) | newAccess); }
 
-    public static final short STATIC       = 0x0008;
-    public static final short FINAL        = 0x0010;
-    public static final short SUPER        = 0x0020;
+    /**
+     * This flag is set on class or interface initialization methods, STATIC class fields, all interface fields, STATIC
+     * methods, and STATIC nested classes.
+     */
+    public static final short STATIC = 0x0008;
+    
+    /**
+     * This flag is set on FINAL classes, FINAL fields and FINAL methods, and is mutually exclusive with {@link
+     * #VOLATILE} and {@link #ABSTRACT}.
+     */
+    public static final short FINAL = 0x0010;
+    
+    /**
+     * This flag is always set on classes, and never set on any other element. Notice that it has the same value as
+     * {@link #SYNCHRONIZED}, which is OK because {@link #SYNCHRONIZED} is for methods and {@link #SUPER} for classes.
+     */
+    public static final short SUPER = 0x0020;
+    
+    /**
+     * This flag is set on SYNCHRONIZED methods. Notice that it has the same value as {@link #SUPER}, which is OK
+     * because {@link #SYNCHRONIZED} is for methods and {@link #SUPER} for classes.
+     */
     public static final short SYNCHRONIZED = 0x0020;
-    public static final short VOLATILE     = 0x0040;
-    public static final short BRIDGE       = 0x0040; // Same value as VOLATILE; a method never has a "volatile" mod.
-    public static final short TRANSIENT    = 0x0080;
-    public static final short VARARGS      = 0x0080; // Same value as TRANSIENT; a method never has a "volatile" mod.
-    public static final short NATIVE       = 0x0100;
-    public static final short INTERFACE    = 0x0200;
-    public static final short ABSTRACT     = 0x0400;
-    public static final short STRICTFP     = 0x0800;
+    
+    /**
+     * This flag is set on VOLATILE fields and is mutually exclusive with {@link #FINAL}. Notice that it has the same
+     * value as {@link #BRIDGE}, which is OK because {@link #BRIDGE} is for methods and {@link #VOLATILE} for fields.
+     */
+    public static final short VOLATILE = 0x0040;
+    
+    /**
+     * This flag is set on 'bridge methods' generated by the compiler. Notice that it has the same value as {@link
+     * #VOLATILE}, which is OK because {@link #BRIDGE} is for methods and {@link #VOLATILE} for fields.
+     */
+    public static final short BRIDGE = 0x0040;
+    
+    /**
+     * This flag is set on TRANSIENT fields. Notice that it has the same value as {@link #VARARGS}, which is OK because
+     * {@link #VARARGS} is for methods and {@link #TRANSIENT} for fields.
+     */
+    public static final short TRANSIENT = 0x0080;
+    
+    /**
+     * This flag is set on 'variable arity' (a.k.a. 'varargs') methods and constructors. Notice that it has the same
+     * value as {@link #TRANSIENT}, which is OK because {@link #VARARGS} is for methods and {@link #TRANSIENT} for
+     * fields.
+     */
+    public static final short VARARGS = 0x0080;
+    
+    /** This flag is set on NATIVE methods, and is mutually exclusive with {@link #ABSTRACT}. */
+    public static final short NATIVE = 0x0100;
+    
+    /**
+     * This flag is set on interfaces (including nested interfaces), and requires that {@link #ABSTRACT} must also be
+     * set. {@link #INTERFACE} is mutually exclusive with {@link #FINAL}, {@link #SUPER} and {@link #ENUM}.
+     */
+    public static final short INTERFACE = 0x0200;
+    
+    /**
+     * This flag is set on all interfaces, ABSTRACT classes and ABSTRACT methods, and is mutually exclusive with
+     * {@link #FINAL}, {@link #NATIVE}, {@link #PRIVATE}, {@link #STATIC} and {@link #SYNCHRONIZED}.
+     */
+    public static final short ABSTRACT = 0x0400;
+    
+    /** This flag is set on STRICTFP methods, and is mutually exclusive with {@link #ABSTRACT}. */
+    public static final short STRICTFP = 0x0800;
 
     // Poorly documented JDK 1.5 modifiers:
 
-    public static final short SYNTHETIC    = 0x1000;
-    public static final short ANNOTATION   = 0x2000;
-    public static final short ENUM         = 0x4000;
+    /**
+     * This flag is set on classes, methods and fields that were generated by the compiler and do not appear in the
+     * source code.
+     */
+    public static final short SYNTHETIC = 0x1000;
     
+    /**
+     * This flag is set on annotation types (including nested annotation types), and requires that {@link #INTERFACE}
+     * is also set.
+     */
+    public static final short ANNOTATION = 0x2000;
+    
+    /**
+     * This flag is set on enumerated types (including nested enumerated types) and enumerated types' elements, and is
+     * mutually exclusive with {@link #INTERFACE}.
+     */
+    public static final short ENUM = 0x4000;
+
+    /**
+     * Composes and returns a string that maps the given modifier as follows:
+     * <ul>
+     *   <li>Value zero is mapped to "".
+     *   <li>Non-zero values are mapped to a sequence of words, separated with blanks.
+     *   <li>{@link #VARARGS} is mapped to "transient", because the two flags have the same value
+     *   <li>{@link #SUPER} is mapped to "synchronized", because the two flags have the same value
+     *   <li>{@link #BRIDGE} is mapped to "volatile", because the two flags have the same value
+     * </ul>
+     */
     public static String
     shortToString(short sh) {
         if (sh == 0) return "";
@@ -87,16 +204,22 @@ class Mod {
         "public",       new Short(Mod.PUBLIC),
         "private",      new Short(Mod.PRIVATE),
         "protected",    new Short(Mod.PROTECTED),
+//      "???",          new Short(Mod.PACKAGE),
         "static",       new Short(Mod.STATIC),
         "final",        new Short(Mod.FINAL),
-//      "super",        new Short(Mod.SUPER),
-        "synchronized", new Short(Mod.SYNCHRONIZED),
-        "volatile",     new Short(Mod.VOLATILE),
-        "transient",    new Short(Mod.TRANSIENT),
+        "synchronized", new Short(Mod.SYNCHRONIZED), // Has the same value as SUPER
+//      "super",        new Short(Mod.SUPER),        // Has the same value as SYNCHRONIZED
+        "volatile",     new Short(Mod.VOLATILE), // Has the same value as BRIDGE
+//      "bridge",       new Short(Mod.BRIDGE),   // Has the same value as VOLATILE
+        "transient",    new Short(Mod.TRANSIENT), // Has the same value as VARARGS
+//      "varargs",      new Short(Mod.VARARGS),   // Has the same value as TRANSIENT
         "native",       new Short(Mod.NATIVE),
         "interface",    new Short(Mod.INTERFACE),
         "abstract",     new Short(Mod.ABSTRACT),
         "strictfp",     new Short(Mod.STRICTFP),
+        "enum",         new Short(Mod.ENUM),
+        "synthetic",    new Short(Mod.SYNTHETIC),
+        "@",            new Short(Mod.ANNOTATION),
     };
 }
 
