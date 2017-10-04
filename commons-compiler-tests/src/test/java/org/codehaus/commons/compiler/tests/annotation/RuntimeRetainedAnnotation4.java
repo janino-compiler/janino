271fa0b360355709b6b045c0afd3bfbe613c87b8
krooijmans <koenrooymans@gmail.com>
2017-10-04 13:44:35 +0200

    added test case for enum field annotations causing StackOverflowErrors

--- commons-compiler-tests/src/test/java/org/codehaus/commons/compiler/tests/annotation/RuntimeRetainedAnnotation4.java
+++ commons-compiler-tests/src/test/java/org/codehaus/commons/compiler/tests/annotation/RuntimeRetainedAnnotation4.java
@@ -26,16 +26,11 @@
 
 package org.codehaus.commons.compiler.tests.annotation;
 
-import static java.lang.annotation.ElementType.CONSTRUCTOR;
-import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.LOCAL_VARIABLE;
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.PARAMETER;
-import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
-
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
  * An annotation type with retention policy "Runtime".
--- commons-compiler-tests/src/test/java/org/codehaus/commons/compiler/tests/annotation/RuntimeRetainedAnnotation4Enum.java
+++ commons-compiler-tests/src/test/java/org/codehaus/commons/compiler/tests/annotation/RuntimeRetainedAnnotation4Enum.java
@@ -26,17 +26,6 @@
 
 package org.codehaus.commons.compiler.tests.annotation;
 
-import static java.lang.annotation.ElementType.CONSTRUCTOR;
-import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.LOCAL_VARIABLE;
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.PARAMETER;
-import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
-
 /**
  * An enum for an annotation type with retention policy "Runtime".
  */
