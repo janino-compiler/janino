9d458af8a47b689b2c9dd4da677f390b5d8300ff
Arno Unkrig <aunkrig@users.noreply.github.com>
2018-12-07 17:12:58 +0100

    Fixed a JAVADOC bug.

--- commons-compiler-jdk/src/main/java/org/codehaus/commons/compiler/jdk/ByteArrayJavaFileManager.java
+++ commons-compiler-jdk/src/main/java/org/codehaus/commons/compiler/jdk/ByteArrayJavaFileManager.java
@@ -58,7 +58,7 @@ import org.codehaus.commons.nullanalysis.NotNullByDefault;
  * to the {@link StandardJavaFileManager}, which stores them in files).
  *
  * @param <M>
- * @see   #getJavaFileForOutput(Location, String, Kind, FileObject)
+ * @see   #getJavaFileForOutput(javax.tools.JavaFileManager.Location, String, Kind, FileObject)
  */
 @NotNullByDefault(false) public
 class ByteArrayJavaFileManager<M extends JavaFileManager> extends ForwardingJavaFileManager<M> {
