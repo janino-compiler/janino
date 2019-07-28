16129fd63412e23c178d7ba85fc475eadc63efed
Arno Unkrig <aunkrig@users.noreply.github.com>
2019-07-28 12:23:14 +0200

    Issue #93: Fix overloaded single static method import Aka pull request #97: Test that reproduces the problem.

---
+++ commons-compiler-tests/src/test/resources/testOverloadedStaticImports/test/SingleStaticImport.java
@@ -0,0 +1,13 @@
+
+package test;
+
+import static test.C2.testOverload;
+
+public
+class SingleStaticImport {
+
+    public void
+    execute() {
+        System.out.println(testOverload((long) 2));
+    }
+}
