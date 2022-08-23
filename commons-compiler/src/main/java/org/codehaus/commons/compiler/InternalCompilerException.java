f969ee10e9e20520eb19cf97117b6a667a57f953
Arno Unkrig <aunkrig@users.noreply.github.com>
2022-08-23 13:49:37 +0200

    InternalCompilerException: Added constructors with additional parameter "location".

--- commons-compiler/src/main/java/org/codehaus/commons/compiler/InternalCompilerException.java
+++ commons-compiler/src/main/java/org/codehaus/commons/compiler/InternalCompilerException.java
@@ -32,7 +32,9 @@ import org.codehaus.commons.nullanalysis.Nullable;
  */
 public
 class InternalCompilerException extends RuntimeException {
-    public InternalCompilerException()                                      {                    }
-    public InternalCompilerException(@Nullable String message)              { super(message);    }
-    public InternalCompilerException(@Nullable String message, Throwable t) { super(message, t); }
+    public InternalCompilerException()                                                                   {                                                                                                           }
+    public InternalCompilerException(@Nullable String message)                                           { super(message);                                                                                           }
+    public InternalCompilerException(@Nullable String message, Throwable t)                              { super(message, t);                                                                                        }
+    public InternalCompilerException(@Nullable Location location, @Nullable String message)              { super(location == null ? message : message == null ? location.toString() : location + ": " + message);    }
+    public InternalCompilerException(@Nullable Location location, @Nullable String message, Throwable t) { super(location == null ? message : message == null ? location.toString() : location + ": " + message, t); }
 }
